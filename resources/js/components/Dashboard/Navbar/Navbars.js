957e56ef8dff32b6113a4c149c66c87ff172760b
sroysawan <sroysawan.kcd@gmail.com>
2023-04-23 14:08:48 +0700

    fixed-top navbar

--- resources/js/components/Dashboard/Navbar/Navbars.js
+++ resources/js/components/Dashboard/Navbar/Navbars.js
@@ -1,22 +1,29 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar ,Nav ,NavDropdown ,Container} from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 import iconImage from "./logo.png";
 import "./navbarStyle.css";
+import { BsEnvelope } from "react-icons/Bs";
+import { VscAccount } from "react-icons/vsc";
+
 class Navbars extends Component {
     render() {
         return (
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar className="fixed-top" collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container fluid>
-                    <Navbar.Brand href="/dashboard">
+                    <Navbar.Brand href="/">
                         <a className="navbar-brand ps-lg-2">
-                            <img src={iconImage} alt="Icon" className="navbar-logo" />
+                            <img
+                                src={iconImage}
+                                alt="Icon"
+                                className="navbar-logo"
+                            />
                         </a>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+                            <Nav.Link href="/">Dashboard</Nav.Link>
                             <NavDropdown title="Staffs" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#operatorlist">
                                     Operator List
@@ -36,6 +43,14 @@ class Navbars extends Component {
                             </NavDropdown>
                             <Nav.Link href="/">Approve</Nav.Link>
                         </Nav>
+                        <ul className="navbar-nav ">
+                            <Nav.Link href="#">
+                                <BsEnvelope />
+                            </Nav.Link>
+                            <Nav.Link href="#">
+                                <VscAccount />
+                            </Nav.Link>
+                        </ul>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
