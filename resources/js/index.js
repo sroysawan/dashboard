d8d5614e4491e571001d7b4c6b18c35ce0d5062b
sroysawan <sroysawan.kcd@gmail.com>
2023-04-26 19:19:28 +0700

    change Route

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,33 +1,40 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom';
-// import ReactDOM from 'react-dom';
-import Home from './components/Home';
-import Operation from './components/Operation';
-import NewTask from './components/NewTask';
-import { DataTable } from 'simple-datatables-classic';
+// import React from 'react';
+// import * as ReactDOM from 'react-dom';
+// import Home from './components/Home';
+// import Operation from './components/Operation';
+// import NewTask from './components/NewTask';
+// import { DataTable } from 'simple-datatables-classic';
+// import DataDashboard from './components/Dashboard/DataDashboard';
+// import DataOperation from './components/Operation/DataOperation';
 
 
-if (document.getElementById('dashboard')) {
-    ReactDOM.render(<Home />, document.getElementById('dashboard'));
-}
+// if (document.getElementById('dashboard')) {
+//     ReactDOM.render(<DataDashboard />, document.getElementById('dashboard'));
+// }
 
-if (document.getElementById('operation')) {
-    ReactDOM.render(<Operation />, document.getElementById('operation'));
-}
+// if (document.getElementById('operation')) {
+//     ReactDOM.render(<DataOperation />, document.getElementById('operation'));
+// }
 
-if (document.getElementById('newtask')) {
-    ReactDOM.render(<NewTask />, document.getElementById('newtask'));
-}
+// if (document.getElementById('newtask')) {
+//     ReactDOM.render(<NewTask />, document.getElementById('newtask'));
+// }
 
-// window.addEventListener('DOMContentLoaded', event => {
-//     // Simple-DataTables
-//     // https://github.com/fiduswriter/Simple-DataTables/wiki
+// App.js
+import React, { Component } from "react";
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import DataDashboard from './components/Dashboard/DataDashboard';
+import DataOperation from './components/Operation/DataOperation';
 
-//     const datatablesSimple = document.getElementById('datatablesSimple');
-//     if (datatablesSimple) {
-//         new simpleDatatables.DataTable(datatablesSimple, {
-//             searchable: false,
-//             paging: false,
-//             fixedHeight: true});
-//     } 
-// });
+ReactDOM.render(
+    <Router>
+      <Routes>
+        <Route path="/" element={<DataDashboard />} />
+        {/* <Route path="/data-dashboard" element={<DataDashboard />} /> */}
+        <Route path="/operation" element={<DataOperation />} />
+        {/* <Route path="/new-task" element={<NewTask />} /> */}
+      </Routes>
+    </Router>,
+    document.getElementById('root')
+  );
