7a1c5b39cf6a114e520a429ccc483f80cce2e0ab
sroysawan <sroysawan.kcd@gmail.com>
2023-05-05 03:07:17 +0700

    Login 05-05-66

---
+++ app/Http/Controllers/loginController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use App\Models\Login;
+use Log;
+use Exception;
+
+
+class loginController extends Controller
+{
+    //
+    public function authLogin(Request $request){
+        try{
+            // return response() -> json('TEST API.');
+            $data=$request->only('id','pass');
+            $user=Login::where('user_id',$data['id'])->first();
+            if(!$user){
+                return response() -> json([
+                    'status'=>false,
+                    'level'=>null,
+                    'message'=>'not found user.',
+                ]);
+            }
+            else{
+                if($user->user_password != $data['pass']){
+                    return response() -> json([
+                        'status'=>false,
+                        'level'=>null,
+                        'message'=>'wrong password.',
+                    ]);
+                }
+                else{
+                    return response() -> json([
+                        'status'=>true,
+                        'level'=>$user->role,
+                        'message'=>'successful.',
+                    ]);
+                }
+            }
+
+        }
+
+        catch(Exception $error){
+            
+            Log::error($error);
+            
+        }
+    }
+
+}
