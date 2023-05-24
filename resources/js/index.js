487557ec2522c3f73e47a8bab44290c60c5bc743
sroysawan <sroysawan.kcd@gmail.com>
2023-05-25 06:13:33 +0700

    Add route Q2 25-05-66

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,50 +1,10 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Operation from './components/Operation';
-// import NewTask from './components/NewTask';
-// import DataDashboard from './components/Dashboard/DataDashboard';
-// import DataOperation from './components/Operation/DataOperation';
-
-
-// if (document.getElementById('dashboard')) {
-//     ReactDOM.render(<DataDashboard />, document.getElementById('dashboard'));
-// }
-
-// if (document.getElementById('operation')) {
-//     ReactDOM.render(<DataOperation />, document.getElementById('operation'));
-// }
-
-// if (document.getElementById('newtask')) {
-//     ReactDOM.render(<NewTask />, document.getElementById('newtask'));
-// }
-
-
-// App.js
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { BrowserRouter as Router, Route,Routes, Switch } from 'react-router-dom';
-// import DataDashboard from './components/Dashboard/DataDashboard';
-// import DataOperation from './components/Operation/DataOperation';
-// import TestOperation from "./components/Operation/TestOperation";
-
-// ReactDOM.render(
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<DataDashboard />} />
-//         {/* <Route path="/data-dashboard" element={<DataDashboard />} /> */}
-//         {/* <Route path="/operation" element={<DataOperation />} /> */}
-//         <Route path="/operation" element={<TestOperation />} />
-//         {/* <Route path="/new-task" element={<NewTask />} /> */}
-//       </Routes>
-//     </Router>,
-//     document.getElementById('root')
-//   );
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DataDashboard from './components/Dashboard/DataDashboard';
 import DataOperation from './components/Operation/DataOperation';
+import DataOperationQ2 from './components/Operation/DataOperationQ2';
 import DataNewTask from './components/NewTask/DataNewtask';
+import DataNewTaskQ2 from './components/NewTask/DataNewTaskQ2';
 import LoginPage from './components/LoginPage/LoginPage';
 import TableOperator from './components/Staff/TableOperator';
 import TableTechnician from './components/Staff/TableTechnician';
@@ -52,6 +12,7 @@ import AddStaff from './components/Staff/AddStaff';
 import Approve from './components/Staff/Approve/Approve';
 import Import from './components/Staff/Import';
 import Table from './components/Staff/Table';
+import ApproveHistory from './components/Staff/Approve/ApproveHistory';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 ReactDOM.render(
@@ -62,13 +23,16 @@ ReactDOM.render(
         <Route path="/login" component={LoginPage} />
         <Route exact path="/" component={DataDashboard} />
         <Route path="/operation" component={DataOperation} />
+        <Route path="/operationQ2" component={DataOperationQ2} />
         <Route path="/newtask" component={DataNewTask} />
+        <Route path="/newtaskQ2" component={DataNewTaskQ2} />
         
         {/* --------> Staff <-------- */}
         <Route path="/operator" component={TableOperator} />
         <Route path="/technician" component={TableTechnician} />
         <Route path="/addstaff" component={AddStaff} />
         <Route path="/approve" component={Approve} />
+        <Route path="/history" component={ApproveHistory} />
         <Route path="/import" component={Import} />
         <Route path="/otherstaff" component={Table} />
       </Switch>
